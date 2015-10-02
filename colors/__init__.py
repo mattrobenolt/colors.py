55189e99e61729b706cc41f092fe9a3c791af5e3
Matt Robenolt <matt@ydekproductions.com>
2015-10-01 19:12:44 -0700

    0.2.1

--- colors/__init__.py
+++ colors/__init__.py
@@ -6,6 +6,6 @@ and generate random colors within boundaries.
 """
 from .base import *
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Matt Robenolt <matt@ydekproductions.com>'
 __license__ = 'BSD'
